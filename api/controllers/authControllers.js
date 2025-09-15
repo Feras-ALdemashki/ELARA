e154e6339701174f7b0da2d87e26c9342656f5dc
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-09-15 18:59:26 +0200

    api config

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -55,7 +55,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
